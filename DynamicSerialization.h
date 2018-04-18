5e1ab734deffe5579307ea63167980d48e071cdd
Neathan <NeathanDev@gmail.com>
2018-04-18 22:30:31 +0200

    Added generic serialization

--- DynamicSerialization.h
+++ DynamicSerialization.h
@@ -15,7 +15,6 @@ namespace DS {
 		return true;
 	}
 
-
 	template<typename Struct, typename Var, typename... Vars>
 	bool createStruct(Struct s, unsigned char& offset, Var var, Vars... vars) {
 		while(!createStruct(s, offset, vars...)) {
@@ -31,6 +30,50 @@ namespace DS {
 		createStruct(s, offset, var, vars...);
 	}
 
+
+	
+	template<typename Struct>
+	char* serialize(Struct s) {
+		size_t size = sizeof(*s);
+		char* data = (char*)malloc(size);
+		memcpy(data, s, size);
+		return data;
+	}
+
+	template<typename Struct>
+	void deserialize(Struct dest, char* data) {
+		memcpy(dest, data, sizeof(*dest));
+	}
+
+
 }
+
+#ifndef DS_NO_FILE
+#include <fstream>
+
+namespace DS {
+
+	template<typename Struct>
+	void serializeToFile(Struct s, const char* fileName) {
+		char* data = serialize(s);
+		std::fstream stream;
+		stream.open(fileName, std::ios::out | std::ios::binary);
+		stream.write(data, sizeof(*s));
+		stream.close();
+	}
+
+	template<typename Struct>
+	void deserializeFromFile(Struct dest, const char* fileName) {
+		char* data = new char;
+		std::fstream stream;
+		stream.open(fileName, std::ios::in | std::ios::binary);
+		stream.read(data, sizeof(*dest));
+		stream.close();
+		deserialize(dest, data);
+	}
+
+}
+
+#endif
 
 #endif
--- Example.cpp
+++ Example.cpp
@@ -12,9 +12,18 @@ int main() {
 	Test* t = new Test();
 
 	DS::createStruct(t, 8, 3.141f, 32);
-
+	
 	std::cout << t->a << std::endl;
 	std::cout << t->b << std::endl;
 	std::cout << t->c << std::endl;
+
+	DS::serializeToFile(t, "temp.dat");
+
+	Test* b = new Test();
+	DS::deserializeFromFile(b, "temp.dat");
+
+	std::cout << b->a << std::endl;
+	std::cout << b->b << std::endl;
+	std::cout << b->c << std::endl;
 	return 0;
 }
